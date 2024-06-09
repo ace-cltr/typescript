7682040a263967a7a94a631e5a65151575b21f89
ace-cltr <anand.vish201@gmail.com>
2024-06-09 12:32:07 +0530

    typescript basics

---
+++ test.ts
@@ -0,0 +1,17 @@
+let x = "hello";
+
+console.log(x + "friend");
+
+// defining function type in typescript
+function takeInput(age: number) {
+  console.log(age);
+}
+takeInput(22);
+
+// defining function return type in typescript
+function twoSum(a: number, b: number) {
+  return a + b;
+}
+
+// typescript provides some basic type :-
+// number, string, boolean, null, undefined
